d713d785bc5b5298740c157bd4b71c44080d8ccf
Ayush Jaiswal <aj7984435@gmail.com>
2025-12-17 20:05:16 +0530

    added zod schema

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,16 +1,34 @@
 import exppress, { json } from "express"
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
-import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware.js";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = exppress();
 
 app.post("/signup", (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
 
+    res.json({
+        userId: "123"
+    })
 })
 
 app.post("/signin", (req, res) => {
-    
+
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -23,6 +41,15 @@ app.post("/signin", (req, res) => {
 })
 
 app.post("/room", middleware, (req, res) => {
+
+    const data = CreateRoomSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json({
+            message: "Incorrect Inputs"
+        })
+        return;
+    }
+
     res.json({
         roonId: 123
     })
